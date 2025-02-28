7d0bd812ec36c6963bb1dbcca2d9c9f953e891f0
nigra <19545604+NOVAinc@users.noreply.github.com>
2025-02-28 02:41:28 +0100

    Implement winning logic

--- script.js
+++ script.js
@@ -8,37 +8,90 @@
   const player2 = Player(prompt("Player 2, what is your piece?"));
 
   let gameOver = false;
+  let winner;
   let currentTurn = Math.random() < 0.5 ? player1.piece : player2.piece;
 
   console.log(`Player 1: ${player1.piece}`);
   console.log(`Player 2: ${player2.piece}`);
-  console.log(`First player: ${currentTurn}`);
+  console.log(`First turn: ${currentTurn}`);
 
   while (!gameOver) {
-    let coordinate;
+    console.log(`Current board: 
+    ${board[0]} | ${board[1]} | ${board[2]}
+    ${board[3]} | ${board[4]} | ${board[5]}
+    ${board[6]} | ${board[7]} | ${board[8]}`);
 
-    coordinate =
+    isGameOn(board);
+
+    let position =
       player1.piece == currentTurn
-        ? prompt(
-            `Player 1, where would you like to place your ${player1.piece} next? Input a number between 1-9`
-          )
-        : prompt(
-            `Player 2, where would you like to place your ${player2.piece} next? Input a number between 1-9`
-          );
+        ? getNewPositionFromPlayer(player1.piece)
+        : getNewPositionFromPlayer(player2.piece);
 
     currentTurn == player1.piece
-      ? playTurn(coordinate, player1.piece)
-      : playTurn(coordinate, player2.piece);
+      ? playTurn(position, player1.piece)
+      : playTurn(position, player2.piece);
   }
 
-  function playTurn(coordinate, piece) {
-    if (board[coordinate]) {
-      console.log(`spot ${coordinate} taken, can't play`);
+  function getNewPositionFromPlayer(piece) {
+    return (
+      prompt(
+        `Where would you like to place your ${piece} next? Input a number between 1-9`
+      ) - 1
+    );
+  }
+
+  function isGameOn(board) {
+    const testBoard = [...board];
+
+    const winningPositions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    winningPositions.map((positions) => {
+      console.log(`Testing ${testBoard} against ${positions}`);
+
+      if (
+        !gameOver &&
+        testBoard[positions[0]] != null &&
+        testBoard[positions[0]] == testBoard[positions[1]] &&
+        testBoard[positions[1]] == testBoard[positions[2]]
+      ) {
+        gameOver = true;
+        winner =
+          testBoard[positions[0]] == player1.piece
+            ? player1.piece
+            : player2.piece;
+        return;
+      } else if (!testBoard.includes(null)) {
+        gameOver = true;
+        winner = "Tie";
+      }
+    });
+
+    if (gameOver) {
+      console.log(`Game over. Winner: ${winner}`);
+      return false;
     } else {
-      board[coordinate] = piece;
+      return true;
+    }
+  }
+
+  function playTurn(position, piece) {
+    if (board[position]) {
+      console.log(`spot ${position} taken, can't play`);
+    } else {
+      board[position] = piece;
       currentTurn =
         currentTurn == player1.piece ? player2.piece : player1.piece;
-      console.log(`played ${piece} at ${coordinate}`);
+      console.log(`played ${piece} at ${position}`);
     }
   }
 })();
